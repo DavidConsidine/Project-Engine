d0b18c9397706483661df01ccc07e3ccafb99574
David Considine <dmconsie@gmail.com>
2019-07-22 12:28:57 +0100

    Added transforms.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,7 @@
 #include <ProjectEngine.h>
 
 #include "imgui/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public ProjectEngine::Layer
 {
@@ -32,10 +33,10 @@ public:
 		m_SquareVA.reset(ProjectEngine::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-				-0.75f, -0.75f, 0.0f,
-				 0.75f, -0.75f, 0.0f,
-				 0.75f,  0.75f, 0.0f,
-				-0.75f,  0.75f, 0.0f
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.5f,  0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<ProjectEngine::VertexBuffer> squarevertexBuffer;
@@ -58,6 +59,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -66,7 +68,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -93,13 +95,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -146,7 +149,7 @@ public:
 		{
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 		}
-
+		
 		ProjectEngine::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
 		ProjectEngine::RenderCommand::Clear();
 
@@ -154,8 +157,19 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 
 		ProjectEngine::Renderer::BeginScene(m_Camera);
-
-		ProjectEngine::Renderer::Submit(m_BlueShader, m_SquareVA);
+				
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				ProjectEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		ProjectEngine::Renderer::Submit(m_Shader, m_VertexArray);
 
 		ProjectEngine::Renderer::EndScene();
