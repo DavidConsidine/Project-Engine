625f458d24243a3aa33caf41b378ce290fbe4023
David Considine <dmconsie@gmail.com>
2019-11-18 13:14:36 +0000

    Minor fixes and Changes.

--- ProjectEngine/src/Platform/Windows/WindowsWindow.cpp
+++ ProjectEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -9,7 +9,7 @@
 
 namespace ProjectEngine
 {
-	static bool s_GLFWInitialized = false;
+	static uint8_t s_GLFWInitialized = 0;
 
 	static void GLFWErrorCallback(int error, const char* description)
 	{
@@ -40,17 +40,17 @@ namespace ProjectEngine
 
 		PE_CORE_INFO("Creating window {0} ({1}, {2})", m_Data.Title, m_Data.Width, m_Data.Height);
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWInitialized == 0)
 		{
-			// TODO: glfwTerminate on system shutdown
+			PE_CORE_INFO("Intializing GLFW");
 			int success = glfwInit();
 			PE_CORE_ASSERT(success, "Could not initialize GLFW!");
 
 			glfwSetErrorCallback(GLFWErrorCallback);
-			s_GLFWInitialized = true;
 		}
 
 		m_Window = glfwCreateWindow((int)m_Data.Width, (int)m_Data.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		++s_GLFWInitialized;
 		
 		m_Context = CreateScope<OpenGLContext>(m_Window);
 		m_Context->Init();
@@ -148,6 +148,12 @@ namespace ProjectEngine
 	void WindowsWindow::Shutdown()
 	{
 		glfwDestroyWindow(m_Window);
+
+		if (--s_GLFWInitialized == 0)
+		{
+			PE_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 	void WindowsWindow::OnUpdate()
